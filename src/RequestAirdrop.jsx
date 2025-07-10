bddf2c978d87e76ee0cfd8b7783483a1e7d8cc99
chandan_ds <chandandhanush273747@gmail.com>
2025-07-10 11:06:37 +0530

    implemented faucet

--- src/RequestAirdrop.jsx
+++ src/RequestAirdrop.jsx
@@ -1,15 +1,24 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 
-export function RequestAirdrop(){
 
-    function requestAirdrop(){
-        alert("Hi there");
-    }
+export function RequestAirdrop() {
+  const wallet = useWallet();
+  const {connection}= useConnection();
 
-    return(
-        <div>
-            <input type="text" placeholder="Amount..." />
-            <button onClick={requestAirdrop}>Request Airdrop</button>
-        </div>
-    )
+  function requestAirdrop() {
+    const publicKey = wallet.publicKey;
+    const amount = document.getElementById("amount").value;
+    connection.requestAirdrop(publicKey, amount * LAMPORTS_PER_SOL );
+  }
+
+  return (
+    <div>
+      <input id="amount" type="text" placeholder="Amount..." /> <br />
+      <button onClick={requestAirdrop}>Request Airdrop</button> <br /> <br />
+      <div> {wallet.publicKey?.toBase58()}</div>
+     
+    </div>
+  );
 }
